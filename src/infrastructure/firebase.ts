dd68e404197e914b5ef31bfa5036d08be0bf3ce3
Toranosuke Minamikawa <tigertora7571@gmail.com>
2024-08-14 18:34:50 +0900

    fix: funch deploy

--- src/infrastructure/firebase.ts
+++ src/infrastructure/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,5 +22,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const database = getDatabase(app);
+const provider = new GoogleAuthProvider();
 
-export { auth, database };
+export { auth, database, provider };
