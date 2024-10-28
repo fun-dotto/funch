96c0f568280195d2a06d7d9546fade16186659a8
Toranosuke Minamikawa <tigertora7571@gmail.com>
2024-10-28 23:05:31 +0900

    fix: dnd

--- src/components/Edit.tsx
+++ src/components/Edit.tsx
@@ -1,8 +1,27 @@
 // import React from "react";
 import { useParams } from "react-router-dom";
+import { FaTrashAlt } from "react-icons/fa";
+import { FC, ReactNode, useState } from "react";
+import { getCategoryMenu, importMenu, Menu } from "../repository/menu";
+import {
+  DndContext,
+  DragOverlay,
+  useDraggable,
+  useDroppable,
+} from "@dnd-kit/core";
 
 const Edit = () => {
   const { year, month } = useParams();
+  const [menuData, setMenuData] = useState(new Map<Date, Menu[]>());
+  const [activeMenu, setActiveMenu] = useState<Menu | null>(null);
+  const categoryOptions = [
+    { value: "1", label: "主菜" },
+    { value: "2", label: "副菜・サラダ" },
+    { value: "4", label: "丼物・カレー" },
+    { value: "11", label: "麺類" },
+    { value: "8", label: "汁物" },
+    { value: "10", label: "デザート" },
+  ];
   let canView = true;
   let targetYear = 0;
   let targetMonth = -1;
@@ -57,7 +76,9 @@ const Edit = () => {
   for (i = 0; i < 7 * 5; i++) {
     const pushDate = new Date(calendarStartDate);
     pushDate.setDate(pushDate.getDate() + i);
-    calendar.push(pushDate);
+    if (pushDate.getDay() > 0 && pushDate.getDay() < 6) {
+      calendar.push(pushDate);
+    }
   }
   const checkDate = new Date(calendarStartDate);
   checkDate.setDate(checkDate.getDate() + i);
@@ -65,61 +86,138 @@ const Edit = () => {
     for (; i < 7 * 6; i++) {
       const pushDate = new Date(calendarStartDate);
       pushDate.setDate(pushDate.getDate() + i);
-      calendar.push(pushDate);
+      if (pushDate.getDay() > 0 && pushDate.getDay() < 6) {
+        calendar.push(pushDate);
+      }
     }
   }
 
-  const calendarWeekStr = ["月", "火", "水", "木", "金", "土", "日"];
+  const calendarWeekStr = ["月", "火", "水", "木", "金"];
 
   return (
     <>
-      <h2>
-        {yearJST}
-        {monthJST}
-      </h2>
-      <div className="my-2 mx-auto">
-        <div className="grid grid-cols-7 justify-items-stretch text-left gap-0.5">
-          {calendarWeekStr.map((v) => (
-            <div className="w-full bg-gray-200 border-gray-300 rounded p-2 text-center">
-              {v}
+      <DndContext
+        onDragStart={(event) => {
+          const { active } = event;
+          if (active == null) {
+            return;
+          }
+          console.log(active);
+          if (active.data.current != null) {
+            setActiveMenu(() => active.data.current!.menu);
+          }
+        }}
+        onDragEnd={(event) => {
+          const { over } = event;
+          if (over == null) {
+            return;
+          }
+          console.log(over);
+          setActiveMenu(() => null);
+        }}
+      >
+        <div className="sm:mr-96 z-10">
+          <h2>
+            {yearJST}
+            {monthJST}
+          </h2>
+          <div className="my-2 mx-auto">
+            <div className="grid grid-cols-5 justify-items-stretch text-left gap-0.5">
+              {calendarWeekStr.map((v) => (
+                <div className="w-full bg-gray-200 border-gray-300 rounded p-2 text-center">
+                  {v}
+                </div>
+              ))}
+
+              {calendar.map((v) => (
+                <div className="w-full bg-gray-200 border-gray-300 rounded p-2">
+                  {v >= monthStartDay && v <= monthEndDay && (
+                    <Droppable date={v} id={v.toISOString().split("T")[0]}>
+                      {new Intl.DateTimeFormat("ja-JP", dayOptions).format(v)}
+                      <div className="flex items-center">
+                        {/* <SelectMenu /> */}
+                      </div>
+                    </Droppable>
+                  )}
+                </div>
+              ))}
             </div>
-          ))}
-          {calendar.map((v) => (
-            <div className="w-full bg-gray-200 border-gray-300 rounded p-2">
-              {v >= monthStartDay && v <= monthEndDay && (
-                <>
-                  {new Intl.DateTimeFormat("ja-JP", dayOptions).format(v)}
-                  <div>
-                    <select name="0000" id="0000">
-                      <option value=""></option>
-                      <option value="主菜">主菜</option>
-                      <option value="副菜・サラダ">副菜・サラダ</option>
-                      <option value="丼物・カレー">丼物・カレー</option>
-                      <option value="麺類">麺類</option>
-                      <option value="ごはん">ごはん</option>
-                      <option value="汁物">汁物</option>
-                      <option value="デザート">デザート</option>
-                    </select>
-                    ：
-                    <select name="0001" id="0001">
-                      <option value=""></option>
-                      <option value="チキン竜田丼">チキン竜田丼</option>
-                      <option value="さらさらトン茶">さらさらトン茶</option>
-                      <option value="鶏から南蛮丼">鶏から南蛮丼</option>
-                      <option value="塩ダレ唐揚げ丼">塩ダレ唐揚げ丼</option>
-                    </select>
-                  </div>
-                </>
-              )}
+            <div className="mt-12">
+              <h3>{monthJST}の共通メニュー</h3>
             </div>
-          ))}
+          </div>
         </div>
-        <div>
-          <h3>{monthJST}の共通メニュー</h3>
-        </div>
-      </div>
+        <aside className="fixed top-0 right-0 w-96 h-screen bg-white overflow-x-hidden overflow-y-scroll z-10">
+          {importMenu().map((m) => {
+            return <Draggable id={String(m.item_code)} menu={m} />;
+          })}
+        </aside>
+        <DragOverlay>
+          {activeMenu && <DraggableBlockSource menu={activeMenu} />}
+        </DragOverlay>
+      </DndContext>
     </>
   );
 };
 
 export default Edit;
+
+type DraggableBlockSourceType = {
+  isDragging?: boolean;
+  menu: Menu;
+};
+
+export const DraggableBlockSource: FC<DraggableBlockSourceType> = ({
+  isDragging,
+  menu,
+}) => {
+  return (
+    <div
+      className={`z-30 p-2 m-1 border rounded text-center bg-white select-none w-fit ${
+        isDragging ? "cursor-grabbing" : "cursor-grab"
+      }`}
+    >
+      {menu.display_name}
+    </div>
+  );
+};
+
+type DraggableProps = {
+  id: string;
+  menu: Menu;
+};
+
+export const Draggable: FC<DraggableProps> = ({ id, menu }) => {
+  // useDraggableを使って必要な値をもらう
+  const { setNodeRef, listeners, attributes, isDragging } = useDraggable({
+    id,
+    data: { menu },
+  });
+
+  return (
+    <div ref={setNodeRef} {...attributes} {...listeners} className="z-20 h-fit">
+      <DraggableBlockSource isDragging={isDragging} menu={menu} />
+    </div>
+  );
+};
+
+type DroppableProp = {
+  date: Date;
+  children: ReactNode;
+  id: string;
+};
+
+export const Droppable: FC<DroppableProp> = ({ date, children, id }) => {
+  const { setNodeRef, isOver } = useDroppable({
+    id,
+  });
+
+  return (
+    <div
+      ref={setNodeRef}
+      className={`h-20 border ${isOver ? "bg-gray-500" : "bg-white"}`}
+    >
+      {children}
+    </div>
+  );
+};
---
+++ src/repository/menu.ts
@@ -0,0 +1,68 @@
+import CoopData from "../assets/menu.json";
+
+
+
+export class Menu{
+  status: number;
+  item_code: number;
+  display_name: string;
+  ingredients: string;
+  price_kumika: number;
+  image_url: string[];
+  is_allergy_unconfirmed: boolean;
+  allergy_codes: number[];
+  category_code: number;
+  group_code: string;
+  is_nutritional_value_unconfirmed: boolean;
+  // nutritionalvalue: Map<string, number>;
+
+  constructor(status: number,
+    item_code: number,
+    display_name: string,
+    ingredients: string,
+    price_kumika: number,
+    image_url: string[],
+    is_allergy_unconfirmed: boolean,
+    allergy_codes: number[],
+    category_code: number,
+    group_code: string,
+    is_nutritional_value_unconfirmed: boolean,
+    // nutritionalvalue: Map<string, number>,
+  ) {
+    this.status = status;
+    this.item_code = item_code;
+    this.display_name = display_name;
+    this.ingredients = ingredients;
+    this.price_kumika = price_kumika;
+    this.image_url = image_url;
+    this.is_allergy_unconfirmed = is_allergy_unconfirmed;
+    this.allergy_codes = allergy_codes;
+    this.category_code = category_code;
+    this.group_code = group_code;
+    this.is_nutritional_value_unconfirmed = is_nutritional_value_unconfirmed;
+    // this.nutritionalvalue = nutritionalvalue;
+  };
+}
+
+export const importMenu = () => {
+  return CoopData.map((data) => {
+    return new Menu(
+      data.status,
+      Number(data.item_code),
+      data.display_name,
+      data.ingredients,
+      data.price_kumika,
+      data.image_url,
+      data.is_allergy_unconfirmed,
+      data.allergy_codes.map((a) => Number(a)),
+      Number(data.category_code),
+      data.group_code,
+      data.is_nutritional_value_unconfirmed,
+      // data.nutritionalvalue,
+    )
+  });
+}
+
+export const getCategoryMenu = (category_code: number) => {
+  return importMenu().filter((m) => m.category_code == category_code);
+}
