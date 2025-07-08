c6a8e448eda9eb432ee7fa8c820a6938fb61c3e8
Hikaru Saito <beretta0602@gmail.com>
2025-07-08 21:48:27 +0900

    コンポーネントの位置調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,39 +29,9 @@ export default function Home() {
       <Header />
       {user ? (
         // ログインしている場合の表示
-        <>
+        <div className="ml-8 mt-[60px]">
           <YearMonthDisplay month={today.getMonth() + 1} />
-          <div>
-            <Link href={`/edit/${today.getFullYear()}/${today.getMonth() + 1}`}>
-              <button
-                type="button"
-                className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-4"
-              >
-                編集
-              </button>
-            </Link>
-          </div>
-          <div>
-            <Link href="/original">
-              <button
-                type="button"
-                className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-4"
-              >
-                未来大 オリジナルメニュー 編集・追加
-              </button>
-            </Link>
-          </div>
-          <div>
-            <Link href="/price">
-              <button
-                type="button"
-                className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-4"
-              >
-                価格変更
-              </button>
-            </Link>
-          </div>
-        </>
+        </div>
       ) : (
         // ログインしていない場合の表示
         <div>
